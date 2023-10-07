82136c6d5764407ce16b53386c6c43277f265c37
jysoriaCompuLequi <2011814@ucc.edu.ar>
2023-10-07 16:24:21 -0300

    Manos a la obra 9

--- Guia 7 - Java/Cosas viendo todo completo/Manos a la obra/ManosALaObra9/src/manosalaobra9/ManosALaObra9.java
+++ Guia 7 - Java/Cosas viendo todo completo/Manos a la obra/ManosALaObra9/src/manosalaobra9/ManosALaObra9.java
@@ -6,6 +6,8 @@ Nota: recordar el uso de la sentencia break.
  */
 package manosalaobra9;
 
+import java.util.Scanner;
+
 /**
  *
  * @author julie
@@ -16,7 +18,24 @@ public class ManosALaObra9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner num = new Scanner(System.in);
+        int numero, contador = 0, suma = 0;
+        
+        do{
+            System.out.println("Ingrese un numero:");
+            numero = num.nextInt();
+            contador += 1;          
+            if (numero == 0){
+                System.out.println("Se capturo el numero cero");
+                break;
+            } else if (numero < 0){
+                continue;
+            } else {
+                suma += numero;
+            }
+        } while (contador != 20);
+        
+        System.out.println("La suma de los numeros ingresados es de: " + suma);
     }
     
 }
