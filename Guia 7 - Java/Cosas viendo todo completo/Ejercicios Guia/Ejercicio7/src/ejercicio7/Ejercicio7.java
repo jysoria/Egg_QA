e0c103cb9b6bc911096fc088ea8cf14cdcd89d7a
jysoria <julieta.jl.js@gmail.com>
2023-11-12 19:15:14 -0300

    creando archivo ej 7

---
+++ Guia 7 - Java/Cosas viendo todo completo/Ejercicios Guia/Ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejercicio7;
+
+/**
+ *
+ * @author Expression JuliS is undefined on line 12, column 14 in Templates/Classes/Main.java.
+ */
+public class Ejercicio7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
