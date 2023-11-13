d675022fcec7c11272ac0d1857d495d5833e6e81
jysoria <julieta.jl.js@gmail.com>
2023-11-13 20:39:17 -0300

    Finalizado ej7

--- Guia 7 - Java/Cosas viendo todo completo/Ejercicios Guia/Ejercicio7/src/ejercicio7/Ejercicio7.java
+++ Guia 7 - Java/Cosas viendo todo completo/Ejercicios Guia/Ejercicio7/src/ejercicio7/Ejercicio7.java
@@ -1,10 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Crear un programa que pida una frase y si esa frase es igual a “eureka” el programa
+    pondrá un mensaje de Correcto, sino mostrará un mensaje de Incorrecto. Nota:
+    investigar la función equals() en Java.
  */
 
 package ejercicio7;
+
+import java.util.Scanner;
 
 /**
  *
@@ -16,7 +18,15 @@ public class Ejercicio7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String fraseUsuario = "", frase = "eureka"; 
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese una frase");
+        fraseUsuario = leer.nextLine();
+        if (frase.equals(fraseUsuario)){
+            System.out.println("Correcto");
+        } else {
+            System.out.println("Incorrecto");
+        }
     }
 
 }
